5dccc586cf7469fff2d01d5fc4588d3e5fc8c6c4
TorySama <xietaorui1999@outlook.com>
2019-11-12 15:30:03 +0800

    sub URL

--- MallProject/backstage/urls.py
+++ MallProject/backstage/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import url
+from django.urls import path
 from backstage.views import index
 
 urlpatterns = [
-    
+    path('', index.index, name='backstage_index'),      # 后台首页
 ]
