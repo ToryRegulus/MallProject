7ce78de7c5bce786a0a5afc2e73c52572ef1ba31
TorySama <xietaorui1999@outlook.com>
2019-11-13 14:26:20 +0800

    update environment

--- MallProject/backstage/urls.py
+++ MallProject/backstage/urls.py
@@ -7,8 +7,7 @@ urlpatterns = [
     path('users/add', users.add, name='backstage_users_add'),
     path('users/insert', users.insert, name='backstage_users_insert'),
     path('users/edit/<int:uid>', users.edit, name='backstage_users_edit'),
-    path(
-        'users/update/<int:uid>', users.update, name='backstage_users_update'
-        ),
+    path('users/update/<int:uid>', users.update,
+         name='backstage_users_update'),
     path('users/del/<int:uid>', users.delete, name='backstage_users_del'),
 ]
