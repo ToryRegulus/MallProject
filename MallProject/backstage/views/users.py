5c9a84be84bf1592b3266312601e953d347e9dd2
TorySama <xietaorui1999@outlook.com>
2019-11-13 16:17:39 +0800

    update footer

--- MallProject/backstage/views/users.py
+++ MallProject/backstage/views/users.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from common.models import Users
+from datetime import datetime
 
 # Create your views here.
 
@@ -18,19 +19,33 @@ def add(request):
 
 def insert(request):
     """执行添加"""
-    return render(request, 'backstage/index.html')
+    try:
+        ob = Users()
+        ob.username = request.POST['registerUsername']
+        ob.password = request.POST['registerPassword']
+        ob.sex = request.POST['gender']
+        ob.name = request.POST['registerName']
+        ob.email = request.POST['registerEmail']
+        ob.phone = request.POST['registerPhone']
+        ob.address = request.POST['registerAddress']
+        ob.code = request.POST['registerZip']
+        ob.state = 1
+        ob.addtime = datetime.now
+    except Exception as err:
+        print(err)
+        context = {'info': 'Addition Failed'}
 
 
 def edit(request):
     """修改会员信息"""
-    return render(request, 'backstage/index.html')
+    pass
 
 
 def update(request):
     """执行修改"""
-    return render(request, 'backstage/index.html')
+    pass
 
 
 def delete(request):
     """删除会员信息"""
-    return render(request, 'backstage/index.html')
+    pass
