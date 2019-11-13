79b948dff5d99472cc4531a9bec804c0acaa10c3
TorySama <xietaorui1999@outlook.com>
2019-11-13 10:12:32 +0800

    backstage views

--- MallProject/backstage/views/index.py
+++ MallProject/backstage/views/index.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('网站后台管理')
+    return render(request, 'backstage/index.html')      # 网站后台主页面
