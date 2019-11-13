9a907f6c436f2b33c359704da4e1295118ccf280
TorySama <xietaorui1999@outlook.com>
2019-11-13 14:13:46 +0800

    update environment

--- MallProject/backstage/urls.py
+++ MallProject/backstage/urls.py
@@ -3,10 +3,10 @@ from backstage.views import index, users
 
 urlpatterns = [
     path('', index.index, name='backstage_index'),                              # 后台首页
-    path('users/',users.index, name='backstage_users_index'),                   # 会员信息主页
-    path('users/add',users.add, name='backstage_users_add'),                    # 添加会员信息
-    path('users/insert',users.insert, name='backstage_users_insert'),           # 执行添加
-    path('users/edit/<int:uid>',users.edit, name='backstage_users_edit'),       # 修改会员信息
-    path('users/update/<int:uid>',users.update, name='backstage_users_update'), # 执行修改
-    path('users/del/<int:uid>',users.delete, name='backstage_users_del'),       # 删除会员信息
+    path('users/', users.index, name='backstage_users_index'),                   # 会员信息主页
+    path('users/add', users.add, name='backstage_users_add'),                    # 添加会员信息
+    path('users/insert', users.insert, name='backstage_users_insert'),           # 执行添加
+    path('users/edit/<int:uid>', users.edit, name='backstage_users_edit'),       # 修改会员信息
+    path('users/update/<int:uid>', users.update, name='backstage_users_update'), # 执行修改
+    path('users/del/<int:uid>', users.delete, name='backstage_users_del'),       # 删除会员信息
 ]
--- MallProject/backstage/views/users.py
+++ MallProject/backstage/views/users.py
@@ -2,27 +2,34 @@ from django.shortcuts import render
 from common.models import Users
 
 # Create your views here.
+
+
 def index(request):
     """会员信息主页"""
     user_list = Users.objects.all()
     context = {'userslist': user_list}
     return render(request, 'backstage/users/index.html', context)
 
+
 def add(request):
     """添加会员信息"""
     return render(request, 'backstage/index.html')
+
 
 def insert(request):
     """执行添加"""
     return render(request, 'backstage/index.html')
 
+
 def edit(request):
     """修改会员信息"""
     return render(request, 'backstage/index.html')
 
+
 def update(request):
     """执行修改"""
     return render(request, 'backstage/index.html')
+
 
 def delete(request):
     """删除会员信息"""
