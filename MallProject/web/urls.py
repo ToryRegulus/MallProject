c98814eaf8bc41c5ef90b1f884927ba0b8b80f21
TorySama <xietaorui1999@outlook.com>
2019-11-21 17:01:21 +0800

    update frontstage templates

--- MallProject/web/urls.py
+++ MallProject/web/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from web.views import index
 
 urlpatterns = [
-    path('', index.index, name='web_index'),      # 前台首页
+    path('', index.index, name='web_index'),            # 前台首页
+    path('list/', index.lists, name='web_list'),        # 商品列表
+    path('detail/', index.detail, name='web_detail'),   # 商品详情
 ]
--- MallProject/web/views/index.py
+++ MallProject/web/views/index.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
+
+
 def index(request):
-    return HttpResponse('网站前台首页')
+    """商品首页"""
+    return render(request, 'web/index.html')
+
+
+def lists(request, pindex=1):
+    """商品列表页"""
+    return render(request, 'web/list.html')
+
+
+def detail(request, gid):
+    """商品详情页"""
+    return render(request, 'web/detail.html')
