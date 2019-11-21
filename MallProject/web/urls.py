b7fd7ba9cfb7502c6bf2ff47677ad9c46a37fbbc
TorySama <xietaorui1999@outlook.com>
2019-11-21 19:14:54 +0800

    update fronstage page

--- MallProject/web/urls.py
+++ MallProject/web/urls.py
@@ -4,5 +4,5 @@ from web.views import index
 urlpatterns = [
     path('', index.index, name='web_index'),            # 前台首页
     path('list/', index.lists, name='web_list'),        # 商品列表
-    path('detail/', index.detail, name='web_detail'),   # 商品详情
+    path('detail/<int:gid>', index.detail, name='web_detail'),   # 商品详情
 ]
